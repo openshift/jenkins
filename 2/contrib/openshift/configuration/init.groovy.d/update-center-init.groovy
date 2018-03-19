1871271d22ee995034a70dcf8a6f9097b971ceec
gabemontero <gmontero@redhat.com>
2018-03-19 14:32:54 -0400

    move update center init groovy to background thread

--- 2/contrib/openshift/configuration/init.groovy.d/update-center-init.groovy
+++ 2/contrib/openshift/configuration/init.groovy.d/update-center-init.groovy
@@ -1,3 +1,27 @@
 import jenkins.model.Jenkins
-Jenkins.getInstance().getPluginManager().doCheckUpdatesServer()
-Jenkins.getInstance().getUpdateCenter().getCoreSource().getData()
+import hudson.security.ACL;
+import jenkins.security.NotReallyRoleSensitiveCallable;
+
+ACL.impersonate(ACL.SYSTEM, new NotReallyRoleSensitiveCallable<Void, Exception>() {
+    public Void call() throws Exception {
+        Thread thread = new Thread(){
+            public void run(){
+                ACL.impersonate(ACL.SYSTEM, new NotReallyRoleSensitiveCallable<Void, Exception>() {
+                    public Void call() throws Exception {
+                            try {
+                                Jenkins.getInstance().getPluginManager().doCheckUpdatesServer();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            Jenkins.getInstance().getUpdateCenter().getCoreSource().getData();
+                        return null;
+                    }
+                });
+            }
+        };
+        
+        thread.start();
+        return null;
+    }
+});
+
