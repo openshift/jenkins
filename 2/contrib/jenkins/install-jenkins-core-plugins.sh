ff1cfa9e527aa85f89886d9671fc1f3ca0702057
gabemontero <gmontero@redhat.com>
2019-03-11 09:46:00 -0400

    Dockerfile.rhel7 cleanup for hybrid core/plugin install behavior on api.ci

--- 2/contrib/jenkins/install-jenkins-core-plugins.sh
+++ 2/contrib/jenkins/install-jenkins-core-plugins.sh
@@ -13,6 +13,16 @@ else
     yum install -y jenkins-2.* jenkins-2-plugins
     rpm -V jenkins-2.* jenkins-2-plugins
     yum clean all
+    # Remove the base-plugins.txt file because it's only used for Centos
+    # and its presence in the rhel image is confusing.
+    rm /opt/openshift/base-plugins.txt
+    mkdir -p /opt/openshift/plugins
+    # we symlink the rpm installed plugins from /usr/lib/jenkins to /opt/openshift/plugins so that
+    # future upgrades of the image and their RPM install automatically get picked by jenkins;
+    # we use symlinks vs. actual files to delineate whether the user has overridden a plugin (and
+    # by extension taken over its future maintenance)
+    for FILENAME in /usr/lib/jenkins/*hpi ; do ln -s $FILENAME /opt/openshift/plugins/`basename $FILENAME .hpi`.jpi; done
+    chown 1001:0 /usr/lib/jenkins/*hpi
 fi
 
 
