2631145053a0a5b804a7bffa60eeb918b7bb5339
Akram Ben Aissi <akram.benaissi@gmail.com>
2021-01-28 08:58:26 +0100

    Upgrade Jenkins to 2.263.3

--- 2/contrib/jenkins/install-jenkins-core-plugins.sh
+++ 2/contrib/jenkins/install-jenkins-core-plugins.sh
@@ -18,8 +18,8 @@ if [[ "${INSTALL_JENKINS_VIA_RPMS}" == "false" ]]; then
       rm -fr /var/cache/yum/x86_64/7Server/*
       rm -fr /var/cache/yum/x86_64/7Server/ # Clean yum cache otherwise, it will fail if --disablerepos are specified
     fi
-    yum -y $YUM_FLAGS --setopt=tsflags=nodocs --disableplugin=subscription-manager install jenkins-2.235.5
-    rpm -V jenkins-2.235.5
+    yum -y $YUM_FLAGS --setopt=tsflags=nodocs --disableplugin=subscription-manager install jenkins-2.263.3
+    rpm -V jenkins-2.263.3
     yum $YUM_FLAGS clean all
     /usr/local/bin/install-plugins.sh $PLUGIN_LIST
 else
