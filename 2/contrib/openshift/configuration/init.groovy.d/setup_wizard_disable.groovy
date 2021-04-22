8f6fd6fb64fc236ca478ec7677d1d624100a0820
Vibhav Bobade <vibhav.bobde@gmail.com>
2021-04-22 10:23:11 +0530

    remove setup wizard disable script in favor of flag

--- 2/contrib/openshift/configuration/init.groovy.d/setup_wizard_disable.groovy
+++
@@ -1,9 +0,0 @@
-#!groovy
-
-import jenkins.model.*
-import hudson.util.*;
-import jenkins.install.*;
-
-def instance = Jenkins.getInstance()
-
-instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
