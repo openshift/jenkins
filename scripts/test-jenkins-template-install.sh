00ea73d7d0082b5a925298b2e14956244332f36a
Akram Ben Aissi <akram.benaissi@gmail.com>
2021-11-25 15:43:02 +0100

    Change smoke tests so they can run in openshift-ci

--- scripts/test-jenkins-template-install.sh
+++ scripts/test-jenkins-template-install.sh
@@ -17,12 +17,23 @@ ${PYTHON_VENV_DIR}/bin/pip install --upgrade setuptools
 ${PYTHON_VENV_DIR}/bin/pip install --upgrade pip
 # -- Generating a new namespace name
 echo "test-namespace-$(uuidgen | tr '[:upper:]' '[:lower:]' | head -c 8)" > ${OUTPUT_DIR}/test-namespace
+if [[ $OPENSHIFT_CI == true ]]; then
+    # openshift-ci will not allow to create different namespaces, so will do all in the same namespace.
+    oc project -q > ${OUTPUT_DIR}/test-namespace
+fi
 export TEST_NAMESPACE=$(cat ${OUTPUT_DIR}/test-namespace)
-echo "Assigning value to variable TEST_NAMESPACE:"${TEST_NAMESPACE}
+echo "Assigning value to variable TEST_NAMESPACE=${TEST_NAMESPACE}"
 # -- Do clean up & create namespace
 echo "Starting cleanup"
-kubectl delete namespace ${TEST_NAMESPACE} --timeout=45s --wait
-kubectl create namespace ${TEST_NAMESPACE}
+if [[ $OPENSHIFT_CI == true ]]; then
+    oc delete all,rolebindings.authorization.openshift.io,bc,cm,is,pvc,sa,secret -l app=jenkins-ephemeral
+    oc delete all,rolebindings.authorization.openshift.io,bc,cm,is,pvc,sa,secret -l app=jenkins-persistent
+    oc delete all,rolebindings.authorization.openshift.io,bc,cm,is,pvc,sa,secret -l app=openshift-jee-sample
+    oc delete all,rolebindings.authorization.openshift.io,bc,cm,is,pvc,sa,secret -l app=jenkins-pipeline-example
+else
+    kubectl delete namespace ${TEST_NAMESPACE} --timeout=45s --wait
+    kubectl create namespace ${TEST_NAMESPACE}
+fi
 mkdir -p ${LOGS_DIR}/smoke-tests-logs
 mkdir -p ${OUTPUT_DIR}/smoke-tests-output
 touch ${OUTPUT_DIR}/backups.txt
@@ -35,7 +46,9 @@ oc project ${TEST_NAMESPACE}
 # -- Trigger the test
 echo "Starting local Jenkins instance"
 ${PYTHON_VENV_DIR}/bin/pip install -q -r smoke/requirements.txt
-echo "Running smoke tests"
-TEST_NAMESPACE=${TEST_NAMESPACE}
-${PYTHON_VENV_DIR}/bin/behave --junit --junit-directory ${TEST_SMOKE_OUTPUT_DIR} --no-capture --no-capture-stderr smoke/features
-echo "Logs collected at "${TEST_SMOKE_OUTPUT_DIR}
+echo "Running smoke tests in namespace with TEST_NAMESPACE=${TEST_NAMESPACE}"
+echo "Logs will be collected in "${TEST_SMOKE_OUTPUT_DIR}
+${PYTHON_VENV_DIR}/bin/behave --junit --junit-directory ${TEST_SMOKE_OUTPUT_DIR} \
+                              --no-capture --no-capture-stderr \
+                              smoke/features -D project_name=${TEST_NAMESPACE}                           
+echo "Logs collected in "${TEST_SMOKE_OUTPUT_DIR}
