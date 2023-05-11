ca081ffd3d202347222164b95920a734dc6dbfe9
Corey Daley <cdaley@redhat.com>
2023-05-11 14:54:29 -0400

    syncing files from master branch

--- 2/contrib/jenkins/install-jenkins-core-plugins.sh
+++ 2/contrib/jenkins/install-jenkins-core-plugins.sh
@@ -9,6 +9,7 @@ if [[ "${INSTALL_JENKINS_VIA_RPMS}" == "false" ]]; then
     curl https://pkg.jenkins.io/redhat-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
     rpm --import https://pkg.jenkins.io/redhat-stable/jenkins-ci.org.key
     rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
     PLUGIN_LIST="$1"
     echo "Plugin list wil be take from file: " $PLUGIN_LIST
     YUM_FLAGS=" "
