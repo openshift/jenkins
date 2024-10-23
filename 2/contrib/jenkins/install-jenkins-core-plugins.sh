eaa57acf65d5aed2ae4e7c37d7748d5ad5541899
Adam Kaplan <adam.kaplan@redhat.com>
2024-10-23 09:29:19 -0400

    JKNS-463: Containerfile for Konflux
    - Add Containerfile for onboarding openshift/jenkins to Konflux.
    - Script to install jenkins.war updated to skip upstream RPM install if
      we source it from a Konflux-built artifact.
    
    Signed-off-by: Adam Kaplan <adam.kaplan@redhat.com>

--- 2/contrib/jenkins/install-jenkins-core-plugins.sh
+++ 2/contrib/jenkins/install-jenkins-core-plugins.sh
@@ -5,10 +5,6 @@ set -o pipefail
 jenkins_version=$(cat /opt/openshift/jenkins-version.txt)
 echo "Jenkins version: ${jenkins_version}"
 if [[ "${INSTALL_JENKINS_VIA_RPMS}" == false ]]; then
-    curl https://pkg.jenkins.io/redhat-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
-    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins-ci.org.key
-    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
     PLUGIN_LIST="$1"
     echo "Plugin list wil be take from file: " $PLUGIN_LIST
     YUM_FLAGS=" "
@@ -16,17 +12,30 @@ if [[ "${INSTALL_JENKINS_VIA_RPMS}" == false ]]; then
     if [ "$#" == "1" ]; then
         YUM_FLAGS="$1"
     fi
-    YUM_CACHE=/var/cache/yum/x86_64/7Server/
-    if [ -d $YUM_CACHE ]; then
-      rm -fr /var/cache/yum/x86_64/7Server/*
-      rm -fr /var/cache/yum/x86_64/7Server/ # Clean yum cache otherwise, it will fail if --disablerepos are specified
-    fi
-    # Since the recent LTS jenkins update we need to install the 'daemonize' package
-    # which is only available in EPEL, so enable it here
-    yum -y --setopt=tsflags=nodocs --disableplugin=subscription-manager install \
-	    https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-    yum -y $YUM_FLAGS --setopt=tsflags=nodocs --disableplugin=subscription-manager install jenkins-${jenkins_version}
-    yum $YUM_FLAGS clean all
+    # Builds on Konflux will copy `jenkins.war` from a dependent container image to /usr/lib/jenkins
+    # If the file is already present, we skip the upstream RPM installation
+    if [[ -f "/usr/lib/jenkins/jenkins.war" ]]; then
+        echo "jenkins.war already exists, skipping upstream RPM installation"
+    else
+        echo "Installing jenkins.war from upstream RPM"
+        curl https://pkg.jenkins.io/redhat-stable/jenkins.repo -o /etc/yum.repos.d/jenkins.repo
+        rpm --import https://pkg.jenkins.io/redhat-stable/jenkins-ci.org.key
+        rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+        rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+        YUM_CACHE=/var/cache/yum/x86_64/7Server/
+        if [ -d $YUM_CACHE ]; then
+            rm -fr /var/cache/yum/x86_64/7Server/*
+            rm -fr /var/cache/yum/x86_64/7Server/ # Clean yum cache otherwise, it will fail if --disablerepos are specified
+        fi
+        # Since the recent LTS jenkins update we need to install the 'daemonize' package
+        # which is only available in EPEL, so enable it here
+        yum -y --setopt=tsflags=nodocs --disableplugin=subscription-manager install \
+            https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+        yum -y $YUM_FLAGS --setopt=tsflags=nodocs --disableplugin=subscription-manager install jenkins-${jenkins_version}
+        yum $YUM_FLAGS clean all
+    fi    
+    
     /usr/local/bin/install-plugins.sh $PLUGIN_LIST
 else
     yum install -y --disableplugin=subscription-manager jenkins-2.* jenkins-2-plugins
