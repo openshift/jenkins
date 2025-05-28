d876e676d45aea332fd4e26bda002cbd9d0199ef
Vinu K <kevy.vinu@gmail.com>
2025-05-28 16:00:43 +0530

    konflux: Add multiple oc binaries
    - Add oc-tools groovy script
    - Modify Dockerfile to add oc and change base image
    - Modify agent-base base image
    - Modify install-jenkins-core-plugins.sh to change yum repo dir
    
    Signed-off-by: Vinu K <kevy.vinu@gmail.com>

---
+++ 2/contrib/openshift/configuration/init.groovy.d/oc-tools.groovy
@@ -0,0 +1,27 @@
+import jenkins.model.Jenkins
+import com.openshift.jenkins.plugins.OpenShiftClientTools
+
+println "oc tools: configuring ..."
+
+def descriptor = Jenkins.instance.getDescriptor(OpenShiftClientTools)
+
+def toolsToAdd = [
+    "oc-4.16": "/usr/share/openshift/bin/oc-416",
+    "oc-4.17": "/usr/share/openshift/bin/oc-417"
+]
+
+def existing = descriptor.getInstallations() as List
+
+toolsToAdd.each { name, path ->
+    if (!existing.any { it.name == name }) {
+        existing << new OpenShiftClientTools(name, path, [])
+        println "oc tools: added: $name -> $path"
+    } else {
+        println "oc tools: skipped (already exists): $name"
+    }
+}
+
+descriptor.setInstallations(existing.toArray(new OpenShiftClientTools[0]))
+descriptor.save()
+
+println "oc tools: all done!"
