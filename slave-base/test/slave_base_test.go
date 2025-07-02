3548853b23754bab9775e5cc171c825ee2f16324
Kunal Memane <kmemane@redhat.com>
2025-07-02 15:09:37 +0530

    fix: fixed pointer error caused by podman bump

--- slave-base/test/slave_base_test.go
+++ slave-base/test/slave_base_test.go
@@ -53,7 +53,7 @@ var _ = Describe("Base slave testing", func() {
 	It("should contain a runnable oc", func() {
 		var err error
 		sgen := specgen.NewSpecGenerator(imageName, false)
-		sgen.Terminal = true
+		*sgen.Terminal = true
 		sgen.Command = []string{"oc"}
 		sgen.Entrypoint = []string{"/bin/bash", "-l", "-c"}
 		id, err = podmancli.ContainerCreate(sgen)
